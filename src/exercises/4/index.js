3b8ebd3249e32f001acbc29a7d5b529177b913bc
Micha Krapp <github@michakrapp.de>
2018-05-06 12:10:56 +0200

    exercise 4

--- src/exercises/4/index.js
+++ src/exercises/4/index.js
@@ -5,11 +5,11 @@ import React from 'react';
 import { render } from 'react-dom';
 
 function App() {
-	return <h1>Hello World!</h1>;
+	return <h1 id="title">Hello World!</h1>;
 }
 
 const root = document.getElementById( 'root' );
 
-const element = App();
+const element = <App />;
 
 render( element, root );
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 
 // Use for exercises 0 - 6.
 
-require( './exercises/3' );
+require( './exercises/4' );
 
 /*/
 
