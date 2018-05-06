0203d7964d00aae7e3940b69632050a1d237e319
Micha Krapp <github@michakrapp.de>
2018-05-06 12:03:33 +0200

    exercise 1 fix

--- src/exercises/1/index.js
+++ src/exercises/1/index.js
@@ -6,6 +6,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+const root = document.getElementById( 'root' );
+
 const element = React.createElement( 'h1', {
     children: 'Hello World',
     style: {
